c5eb683808153ca087fcd724885435e33b0714a8
rc <pootiswwe@gmail.com>
2023-02-24 12:11:02 +0300

    Update

--- YouTube/__init__.py
+++ YouTube/__init__.py
@@ -2,6 +2,7 @@ import os
 
 import feedparser
 import logging
+import requests
 
 from quart import Blueprint, request, jsonify
 from TelegramBot import telegram_bot
@@ -45,6 +46,9 @@ async def youtube():
             # Отправить в буфер
             videos_buffer.append(video_id)
 
+            # Подписаться заново
+            await youtube_resubscribe()
+
     # В случае необходимости подписаться на обновления
     challenge = request.args.get('hub.challenge')
     if challenge:
@@ -52,3 +56,19 @@ async def youtube():
 
     # В любом другом случае вернуть тут 200
     return jsonify({"status": "OK"}), 200
+
+
+async def youtube_resubscribe():
+    response = requests.post("https://pubsubhubbub.appspot.com/", data={
+        'hub.mode': 'subscribe',
+        'hub.callback': 'https://cattero.dev/youtube',
+        'hub.topic': f'https://www.youtube.com/xml/feeds/videos.xml?channel_id={os.environ["YOUTUBE_ID"]}',
+        'hub.verify': 'async',
+        'hub.verify_token': os.environ["RESUB_TOKEN"]
+    })
+    feed = feedparser.parse(response.text)
+
+    if feed.status == 200 and feed.entries[0].title == 'Verification Request Succeeded':
+        logging.debug("Subscription to hub extended.")
+    else:
+        logging.error(f"Error subscribing to the topic")
